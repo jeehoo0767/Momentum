862fc8243bf90c4c3680304436b06a76228ab27e
jeehoo0767 <jeehoo0767@naver.com>
2020-09-18 02:38:41 +0900

    userName기억 구현

--- gretting.js
+++ gretting.js
@@ -1,37 +1,43 @@
 const form = document.querySelector(".js-form"),
-        input = form.querySelector(".input"),
-        greeting = document.querySelector(".js-greetings");
-const USER_LS = "currentUser", // 로컬스토리지에 넣을 유저 정보
-        SHOWING_CN = "showing";
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
 
-function handleSubmit(event){
-    event.preventdefault()
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
     const currentValue = input.value;
-    console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
 }
 
-function askForName(){
+function askForName() {
     form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);   
+    form.addEventListener("submit", handleSubmit);
 }
 
-function paintGreeting(text){
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 
-function loadName(){
+function loadName() {
     const currentUser = localStorage.getItem(USER_LS); // 로컬스토리지에 유저 정보를 넣는다
-    if(currentUser === null){
-        askForName();   
+    if (currentUser === null) {
+        askForName();
     }
-    else{
+    else {
         paintGreeting(currentUser);
     }
 }
-function init(){
+
+function init() {
     loadName();
 }
 init();
