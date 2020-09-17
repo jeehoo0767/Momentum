213a466354b2562f3636dda0cc5886ddff894dba
jeehoo0767 <66991772+jeehoo0767@users.noreply.github.com>
2020-09-18 01:15:57 +0900

    array->localstorage 저장 작업 수정중

--- todo.js
+++ todo.js
@@ -6,7 +6,20 @@ const TODOS_LS = 'toDos';
 
 const toDos = [];
 
-function saveToDos(){
+function filterFn(toDo){
+    
+}
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){ 
+        return toDo.id !== parseInt(li.id);
+    });
+}
+
+function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
@@ -14,16 +27,17 @@ function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = toDos.length +1;
+    const newId = toDos.length + 1;
+    delBtn.addEventListener("click", deleteToDo);
     delBtn.innerHTML = "&#10060;";
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
-    li.id = newId; 
+    li.id = newId;
     toDoList.appendChild(li);
-    const toDoObj ={
-        text : text,
-        id : newId
+    const toDoObj = {
+        text: text,
+        id: newId
     };
     toDos.push(toDoObj);
     saveToDos();
@@ -40,8 +54,8 @@ function loadToDos() {
     const loadedtoDos = localStorage.getItem(TODOS_LS);
     if (loadedtoDos !== null) {
         const parsedToDos = JSON.parse(loadedtoDos);
-        parsedToDos.forEach(function(toDo){
-            paintToDo(todo.text);
+        parsedToDos.forEach(function (toDo) {
+            paintToDo(toDo.text);
         })
     } else {
 
