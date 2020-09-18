e31d0b2b3f81f7000c4b8cf1c6fc2acebef96ece
jeehoo0767 <66991772+jeehoo0767@users.noreply.github.com>
2020-09-18 15:57:53 +0900

    todoList localStorage저장 구현

--- todo.js
+++ todo.js
@@ -1,34 +1,49 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
-toDoList = document.querySelector(".js-toDoList");
+    toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
-function paintToDo(text){
-    const li = document.createElement("li");
-    const delBtn = document.createElement("button");
-    delBtn.value = "X";
-    const span = document.createElement("span");
-    span.innerText = text;
-    li.appendChild(span);
-    li.appendChild(delBtn);
-    toDoList.appendChild(li);
-    
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
-function handleSubmit(event){
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length +1;
+    delBtn.innerHTML = "&#10060;";
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId; 
+    toDoList.appendChild(li);
+    const toDoObj ={
+        text : text,
+        id : newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event) {
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
     toDoInput.value = "";
 }
 
-function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
-    }
-    else{
+function loadToDos() {
+    const loadedtoDos = localStorage.getItem(TODOS_LS);
+    if (loadedtoDos !== null) {
+        const parsedToDos = JSON.parse(loadedtoDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(todo.text);
+        })
+    } else {
 
     }
 }
