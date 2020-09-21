84776f0b8885a57de82669146b45a05636a88306
jeehoo0767 <66991772+jeehoo0767@users.noreply.github.com>
2020-09-21 16:35:08 +0900

    위치,날씨정보 불러오기

---
+++ weather.js
@@ -0,0 +1,57 @@
+const COORDS = 'coords';
+const API_KEY = "e785f87a9692eb0afd33a76d1cffc09b";
+const weather = document.querySelector(".js-weather");
+
+function getWeather(lat, lng){ // lat 위도 lng 경도 날씨정보 가져오기
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    });
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+    //handleGeoSucces 에서 저장된 위도,경도 객체를 localstorage에 저장.
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;//위도 가져오기
+    const longitude = position.coords.longitude;//경도 가져오기
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    //javascript에서 key명과 value명이 같다면 생략하여 하나만 사용해도 객체 할당
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+    console.log("error");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }
+    else{
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+init();
